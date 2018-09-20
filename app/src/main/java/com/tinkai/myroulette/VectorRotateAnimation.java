3bbde4ce9aa91f9227c769b63b54b276d74db673
tinka <jyamusonkouki@gmail.com>
2018-09-20 11:41:53 +0900

    回転アニメーションの変化を決定

--- app/src/main/java/com/tinkai/myroulette/VectorRotateAnimation.java
+++ app/src/main/java/com/tinkai/myroulette/VectorRotateAnimation.java
@@ -5,16 +5,17 @@ import android.view.animation.Transformation;
 
 public class VectorRotateAnimation extends Animation {
     private RouletteView rouletteView;
-    private int velocity;
+    private float velocity;
 
-    VectorRotateAnimation(RouletteView rouletteView, int velocity) {
+    VectorRotateAnimation(RouletteView rouletteView, float velocity) {
         this.rouletteView = rouletteView;
         this.velocity = velocity;
     }
 
     @Override
     protected void applyTransformation(float interpolatedTime, Transformation transformation) {
-        this.velocity = (int)(this.velocity * (1.0f - interpolatedTime));
+        this.velocity = (this.velocity * 0.98f);
+        if (this.velocity < 0.01f) this.velocity = 0.0f;
 
         this.rouletteView.addRotationAngle(this.velocity);
         this.rouletteView.requestLayout();
