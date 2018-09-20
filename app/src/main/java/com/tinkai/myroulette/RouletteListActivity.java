8460cce8ad69eb22357d42f8065cf9c634377051
tinka <jyamusonkouki@gmail.com>
2018-09-20 12:31:16 +0900

    ルーレットを自作するために必要なファイルを作成。

---
+++ app/src/main/java/com/tinkai/myroulette/RouletteListActivity.java
@@ -0,0 +1,13 @@
+package com.tinkai.myroulette;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouletteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roulette_list);
+    }
+}
