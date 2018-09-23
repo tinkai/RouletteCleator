4355c2339ce22a9d2539db1f32d48a6988171a8f
tinka <jyamusonkouki@gmail.com>
2018-09-23 18:42:28 +0900

    編集したルーレット情報をDB登録することができた。 なんとかできたから、要リファクタリング。

--- app/src/main/java/com/tinkai/myroulette/EditRouletteActivity.java
+++ app/src/main/java/com/tinkai/myroulette/EditRouletteActivity.java
@@ -3,31 +3,44 @@ package com.tinkai.myroulette;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class EditRouletteActivity extends AppCompatActivity {
     RouletteOpenHelper helper = null;
-    boolean newFlag = false;
+    private boolean newFlag = false;
     private String id = "";
+    private ArrayList<LinearLayout> itemList;
+    private ArrayList<EditText> itemNameList;
+    private ArrayList<EditText> itemRatioList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_roulette);
 
+        // DB
         if (helper == null) {
             helper = new RouletteOpenHelper(EditRouletteActivity.this);
         }
 
         Intent intent = this.getIntent();
-        this.id = intent.getStringExtra("id");
+        id = intent.getStringExtra("id");
+
         if (id.equals("")) {
             // 新規作成
             newFlag = true;
@@ -48,7 +61,48 @@ public class EditRouletteActivity extends AppCompatActivity {
             }
         }
 
-        Button registerButton = findViewById(R.id.register_button);
+        // itemList
+        this.itemList = new ArrayList<>();
+        this.itemNameList = new ArrayList<>();
+        this.itemRatioList = new ArrayList<>();
+        if (id.equals("")) {
+            addItemList();
+        } else {
+            SQLiteDatabase db = helper.getWritableDatabase();
+            try {
+                Cursor c = db.rawQuery("select id from ROULETTE_TABLE where uuid = '" + id + "'", null);
+                c.moveToFirst();
+                String rouletteID = String.valueOf(c.getInt(0));
+                c = db.rawQuery("select name, ratio from ROULETTE_ITEM_TABLE" + rouletteID, null);
+                boolean next = c.moveToFirst();
+                while (next) {
+                    String itemName = c.getString(0);
+                    String itemRatio = c.getString(1);
+                    Log.d("log", "==================" + itemName);
+                    Log.d("log", "==================" + itemRatio);
+                    addItemList();
+                    Log.d("log", "==================" + this.itemNameList.size() + " " + this.itemRatioList.size());
+                    this.itemNameList.get(this.itemNameList.size()-1).setText(itemName);
+                    this.itemRatioList.get(this.itemRatioList.size()-1).setText(itemRatio);
+                    next = c.moveToNext();
+                }
+            } finally {
+                db.close();
+            }
+
+        }
+
+
+        // Button
+        Button addItemButton = findViewById(R.id.add_item_button);
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItemList();
+            }
+        });
+
+        final Button registerButton = findViewById(R.id.register_button);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,8 +114,25 @@ public class EditRouletteActivity extends AppCompatActivity {
                     if (newFlag) {
                         id = UUID.randomUUID().toString();
                         db.execSQL("insert into ROULETTE_TABLE(uuid, name) VALUES('" + id + "', '" + name + "')");
+
+                        Cursor c = db.rawQuery("select id from ROULETTE_TABLE where uuid = '" + id + "'", null);
+                        c.moveToFirst();
+                        String rouletteID = String.valueOf(c.getInt(0));
+                        Log.d("log", "=================Roulette" + rouletteID);
+                        db.execSQL("CREATE TABLE ROULETTE_ITEM_TABLE" + rouletteID + "(" +
+                                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                "name TEXT, " +
+                                "ratio TEXT)");
+                        registerItemList(db);
                     } else {
                         db.execSQL("update ROULETTE_TABLE set name = '" + name + "' where uuid = '" + id + "'");
+                        Cursor c = db.rawQuery("select id from ROULETTE_TABLE where uuid = '" + id + "'", null);
+                        c.moveToFirst();
+                        String rouletteID = String.valueOf(c.getInt(0));
+                        Log.d("log", "=================Roulette" + rouletteID);
+                        db.execSQL("delete from ROULETTE_ITEM_TABLE" + rouletteID);
+                        Log.d("log", "=================1");
+                        registerItemList(db);
                     }
                 } finally {
                     db.close();
@@ -71,6 +142,89 @@ public class EditRouletteActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void addItemList() {
+        LinearLayout itemListLayout = findViewById(R.id.item_list);
+
+        LinearLayout itemLayout = new LinearLayout(this);
+        itemLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        itemLayout.setOrientation(LinearLayout.HORIZONTAL);
+        this.itemList.add(itemLayout);
+
+        TextView numText = new TextView(this);
+        String num = String.valueOf(this.itemNameList.size());
+        numText.setText(String.valueOf(this.itemNameList.size()+1) + ".");
+        itemLayout.addView(numText, new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                1
+        ));
+
+        EditText nameEditText = new EditText(this);
+        nameEditText.setText("Name");
+        itemLayout.addView(nameEditText, new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                6
+        ));
+        this.itemNameList.add(nameEditText);
+
+        EditText ratioEditText = new EditText(this);
+        ratioEditText.setText("Ratio");
+        itemLayout.addView(ratioEditText, new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                2
+        ));
+        this.itemRatioList.add(ratioEditText);
+
+        Button deleteButton = new Button(this);
+        deleteButton.setText("Del");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        itemLayout.addView(deleteButton, new LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                2
+        ));
+
+        itemListLayout.addView(itemLayout);
+
+        /*
+        for (int i = 0; i < this.itemNameList.size(); i++) {
+            Log.d("log", String.valueOf(this.itemNameList.get(i).getText()));
+            Log.d("log", String.valueOf(this.itemRatioList.get(i).getText()));
+        }
+        */
+    }
+
+    private void registerItemList(SQLiteDatabase db) {
+        try {
+            Log.d("log", "=================" + id);
+            Cursor c = db.rawQuery("select id from ROULETTE_TABLE where uuid = '" + id + "'", null);
+
+            c.moveToFirst();
+            String rouletteID = String.valueOf(c.getInt(0));
+            Log.d("log", "=================" + rouletteID);
+            for (int i = 0; i < this.itemNameList.size(); i++) {
+                String itemName = String.valueOf(this.itemNameList.get(i).getText());
+                String itemRatio = String.valueOf(this.itemRatioList.get(i).getText());
+                Log.d("log", "=================" + i);
+                Log.d("log", "=================Roulette" + rouletteID);
+                Log.d("log", "=================" + itemName + " " + itemRatio);
+                db.execSQL("insert into ROULETTE_ITEM_TABLE" + rouletteID + "(name, ratio) VALUES('" + itemName + "', '" + itemRatio + "')");
+            }
+        } finally {
+            db.close();
+        }
     }
 
 }
